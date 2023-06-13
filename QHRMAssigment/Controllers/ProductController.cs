c918c4e6950065c0b2a005e497e777e73926825a
Kirti <kirtijadhav2791@gmail.com>
2023-06-13 22:55:32 +0530

    QHRM

---
+++ QHRMAssigment/Controllers/ProductController.cs
@@ -0,0 +1,63 @@
+﻿using Dapper;
+
+using Microsoft.AspNetCore.Mvc;
+using QHRMAssigment.Models;
+using System.Data;
+using System.Linq;
+
+namespace QHRMAssigment.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(DapperCon.ReturnList<AddNewProd>("GetAllProducts"));
+        }
+
+        //for insert operation
+        [HttpGet]
+        public ActionResult AddorUpdate(int id = 0)
+        {
+            if (id == 0)
+                return View();
+            else
+            {
+                DynamicParameters p = new DynamicParameters();
+                p.Add("@SN", id);
+
+                return View(DapperCon.ReturnList<AddNewProd>("ProductViewById", p).FirstOrDefault<AddNewProd>());
+
+            }
+        }
+        [HttpPost]
+        public ActionResult AddorUpdate(AddNewProd prod)
+        {
+            DynamicParameters d = new DynamicParameters();
+            d.Add("@SN", prod.SN);
+            d.Add("@Product", prod.Product);
+            d.Add("@Description", prod.Description);
+            d.Add("@Price", prod.Price);
+            d.Add("@Created", DateTime.Now); //set date and time
+            DapperCon.ExecuteWithoutReturn("AddOrEditProduct", d);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            DynamicParameters d = new DynamicParameters();
+            d.Add("@SN", id);
+            DapperCon.ExecuteWithoutReturn("ProductDeleteById", d);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            DynamicParameters d = new DynamicParameters();
+            d.Add("@SN", id);
+            DapperCon.ExecuteWithoutReturn("ShowProduct", d);
+            return View(DapperCon.ReturnSingle<AddNewProd>("ShowProduct", d));
+        }
+    }
+}
---
+++ QHRMAssigment/Models/DapperCon.cs
@@ -0,0 +1,53 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace QHRMAssigment.Models
+{
+    public class DapperCon
+    {
+        private static string connectionString = @"Data Source=(localdb)\ProjectModels;Initial Catalog=""QHRMDB"";Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+        public static void ExecuteWithoutReturn(string procedureName, DynamicParameters param = null)
+        {
+
+            using (SqlConnection sqlcon = new SqlConnection(connectionString))
+            {
+                sqlcon.Open();
+                sqlcon.Execute(procedureName, param, commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public static T ExecuteReturnScaler<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlcon = new SqlConnection(connectionString))
+            {
+                sqlcon.Open();
+                return (T)Convert.ChangeType(sqlcon.ExecuteScalar(procedureName, param, commandType: CommandType.StoredProcedure), typeof(T));
+            }
+        }
+        public static IEnumerable<T> ReturnList<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlcon = new SqlConnection(connectionString))
+            {
+                sqlcon.Open();
+                return sqlcon.Query<T>(procedureName, param, commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public static T ReturnSingle<T>(string storedProcedureName, DynamicParameters parameters)
+        {
+            using (var sqlcon = new SqlConnection(connectionString))
+            {
+                sqlcon.Open();
+                return sqlcon.QuerySingleOrDefault<T>(storedProcedureName, parameters, commandType: CommandType.StoredProcedure);
+            }
+        }
+
+
+        internal static void ExecuteWithoutReturn(string v, object parameters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
